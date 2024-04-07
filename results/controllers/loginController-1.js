ef7eba6d25b097b21e21d7e074f609bdf76dbe99
Lee Kyuho <64120854+kyuhho@users.noreply.github.com>
2024-04-08 00:11:49 +0900

    chore: delete result directory

--- results/controllers/loginController-1.js
+++
@@ -1,9 +0,0 @@
-const asyncHandler = require("express-async-handler");
-
-//@desc Get login page
-//@route GET /
-const getLogin = (req, res) => {
-  res.render("home");
-};
-
-module.exports = { getLogin };
