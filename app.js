87dbbc87630cf7b53dcb8e9fbd974e77e78b13e8
kyuhho <rbgh0114@naver.com>
2024-04-21 21:01:39 +0900

    feat: update 24/04/21

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
 const express = require("express");
 //app server 객체 생성
 const app = express();
-const errorHandler = require("./middlewares/errorHandler");
+
 const port = 3000;
 
-// /로 요청했을 때 라우트 코드
-app.get("/", (req, res) => {
-  res.status(200).send("hello express!");
+const requestTime = (res, req, next) => {
+  let today = new Date();
+  let now = today.toLocaleTimeString();
+  req.requestTime = now;
+  next();
+};
+
+app.use(requestTime);
+
+app.route("/").get((req, res) => {
+  const responseText = `요청 시간 ${res.requestTime}`;
+  res.set("Content-type", "text/plain");
+  res.status(200).send(responseText);
 });
-
-app.get("/test", (req, res, next) => {
-  const error = new Error("test");
-  error.status = 401;
-  next(error);
-});
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 // /contacts로 요청했을 때의 라우트 코드
 app.use("/contacts", require("./routes/contactRoutes"));
-
-app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`port: ${port} server executed`);
